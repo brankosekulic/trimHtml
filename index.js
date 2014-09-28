e7e55e7c8b56e304ae07e606eb229774893e5c84
Branko Sekulic <brankodd@gmail.com>
2014-09-28 13:20:55 +0200

    more link

--- index.js
+++ index.js
@@ -4,101 +4,107 @@
  * @param  {Object} options
  * @return {Object}
  */
-function trimHtml(html, options){
+function trimHtml(html, options) {
 
     options = options || {};
 
     var limit = options.limit || 100,
         preserveTags = (typeof options.preserveTags !== 'undefined') ? options.preserveTags : true,
-        sufix = options.sufix || '...';
+        sufix = options.sufix || '...',
+        moreLink = options.moreLink || '';
 
     var arr = html.replace(/</g, "\n<")
-                  .replace(/>/g, ">\n")
-                  .replace(/\n\n/g, "\n")
-                  .replace(/^\n/g, "")
-                  .replace(/\n$/g, "")
-                  .split("\n");
-    
+        .replace(/>/g, ">\n")
+        .replace(/\n\n/g, "\n")
+        .replace(/^\n/g, "")
+        .replace(/\n$/g, "")
+        .split("\n");
+
     var sum = 0,
         row, cut, add,
         tagMatch,
         tagName,
         tagStack = [],
         more = false;
-                                                         
-    for(var i = 0; i < arr.length; i++){
-    
+
+    for (var i = 0; i < arr.length; i++) {
+
         row = arr[i].replace(/[ ]+/g, ' ');
-    
-        if(!row.length){
-          continue;
+
+        if (!row.length) {
+            continue;
         }
 
-        if(row[0] !== "<"){
-        
-            if(sum >= limit){
-                row = "";   
-            }else if((sum + row.length) >= limit) {
+        if (row[0] !== "<") {
+
+            if (sum >= limit) {
+                row = "";
+            } else if ((sum + row.length) >= limit) {
 
                 cut = limit - sum;
 
-                if(row[cut - 1] === ' '){
-                  cut -= 1;
-                }else{
+                if (row[cut - 1] === ' ') {
+                    cut -= 1;
+                } else {
 
-                  add = row.substring(cut).split('').indexOf(' ');
+                    add = row.substring(cut).split('').indexOf(' ');
 
-                  if(add !== -1){
-                    cut += add;
-                  }else{
-                    cut = row.length;
-                  }
+                    if (add !== -1) {
+                        cut += add;
+                    } else {
+                        cut = row.length;
+                    }
                 }
 
                 row = row.substring(0, cut) + sufix;
+
+                if (moreLink) {
+                    row += '<a href="' + moreLink + '" style="display:inline">»</a>';
+                }
+
                 sum = limit;
                 more = true;
-            }else{
+            } else {
                 sum += row.length;
             }
-        }else if(!preserveTags){
+        } else if (!preserveTags) {
             row = '';
-        }else if(sum >= limit){
+        } else if (sum >= limit) {
 
-          tagMatch = row.match(/[a-zA-Z]+/);
-          tagName = tagMatch ? tagMatch[0]: '';
+            tagMatch = row.match(/[a-zA-Z]+/);
+            tagName = tagMatch ? tagMatch[0] : '';
 
-          if(tagName){
-            if(row.substring(0, 2) !== '</'){
+            if (tagName) {
+                if (row.substring(0, 2) !== '</') {
 
-              tagStack.push(tagName);
-              row = '';
-            }else{
+                    tagStack.push(tagName);
+                    row = '';
+                } else {
 
-              while(tagStack[tagStack.length - 1] !== tagName && tagStack.length){
-                tagStack.pop();
-              }
+                    while (tagStack[tagStack.length - 1] !== tagName && tagStack.length) {
+                        tagStack.pop();
+                    }
 
-              if(tagStack.length){
+                    if (tagStack.length) {
+                        row = '';
+                    }
+
+                    tagStack.pop();
+                }
+            } else {
                 row = '';
-              }
-
-              tagStack.pop();
             }
-          }else{
-            row = '';
-          }
         }
 
         arr[i] = row;
     }
-    
+
     return {
-      html: arr.join("\n").replace(/\n/g, ""),
-      more: more
+        html: arr.join("\n").replace(/\n/g, ""),
+        more: more
     };
 }
 
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = trimHtml;
+    module.exports = trimHtml;
 }
