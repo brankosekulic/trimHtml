0457f8920b43766548849718251b73a2913ca6ff
Branko Sekulic <brankodd@gmail.com>
2014-09-30 23:54:04 +0200

    .travis.yml

--- tests/index.js
+++ tests/index.js
@@ -3,16 +3,22 @@ var trimHtml = require('../index.js'),
     path = require('path'),
     fixtures = path.resolve('tests/fixtures');
 
-exports.testFixtures = function(test){
+exports.testFixtures = function(test) {
 
-    fs.readdir(fixtures + '/inputs/',function(err,files){
+    fs.readdir(fixtures + '/inputs/', function(err, files) {
 
-        files.forEach(function(file){
+        files.forEach(function(file) {
 
-            var input = fs.readFileSync(fixtures + '/inputs/' + file, { encoding: 'utf-8'}),
-                output = fs.readFileSync(fixtures + '/outputs/' + file, { encoding: 'utf-8'});
+            var input = fs.readFileSync(fixtures + '/inputs/' + file, {
+                    encoding: 'utf-8'
+                }),
+                output = fs.readFileSync(fixtures + '/outputs/' + file, {
+                    encoding: 'utf-8'
+                });
 
-            test.equal(trimHtml(input, { limit: 500}).html, output, "Fixture " + file + " not match..");
+            test.equal(trimHtml(input, {
+                limit: 500
+            }).html, output, "Fixture " + file + " not match..");
         });
 
         test.expect(files.length);
